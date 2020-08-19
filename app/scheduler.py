b6b15a765c319156b3dc722dc08232e3f2ff19d8
Julia Caffrey <jscaffrey@gmail.com>
2020-08-18 21:21:23 -0400

    Reset all locations to 0 overnight

--- app/scheduler.py
+++ app/scheduler.py
@@ -15,8 +15,17 @@ def commit_occupancy():
     
     db.session.commit()
 
+def auto_reset_to_zero():
+    locations = Location.query.all()
+    dt = datetime.datetime.now()
+    for location in locations:
+        location.occupancy = 0;
+
+    db.session.commit()
+
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=commit_occupancy, trigger="cron", hour="*", minute="25")
+scheduler.add_job(func=auto_reset_to_zero, trigger="cron", hour="3", minute="11")
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
